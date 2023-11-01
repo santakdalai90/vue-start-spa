21a12fa8307ec2bc118f352d4a668f4c552b4c01
Santak Dalai <santakdalai90@live.com>
2023-11-01 08:31:49 +0530

    using composition api

--- src/views/PagesList.vue
+++ src/views/PagesList.vue
@@ -1,3 +1,30 @@
 <template>
-    <p>This is the pages list</p>
+    <p>{{counter}}</p>
+    <button @click.prevent="click">Click</button>
 </template>
+
+<script setup>
+// export default ({
+//     data() {
+//         return {
+//             counter: 0
+//         }
+//     },
+//     methods: {
+//         click() {
+//             this.counter++
+//         }
+//     }
+// })
+
+import {ref, reactive} from 'vue'
+
+const counter = ref(0)
+const data = reactive({counter:0})
+
+function click() {
+    counter.value++
+    // data.counter++
+}
+</script>
+
